a88fa610c1601d3c4a086eec9aad8bb6534159d3
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2026-01-28 13:37:58 -0800

    moar item

---
+++ Assets/Scripts/RianScripts/lok.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class lok : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
