04177ba37dd64c7d5ad394620e7893bd1359712d
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2026-01-28 11:32:22 -0800

    player is working

---
+++ Assets/Scripts/RianScripts/PlayerMovement.cs
@@ -0,0 +1,133 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float damage;
+    [SerializeField] private GameObject gun;
+
+   
+
+    [SerializeField] private Animator anim;
+
+    public float speed = 5f;
+    public Vector3 offset;
+    public Vector2 boxsize;
+    public float castDistance;
+    private Vector2 movementInput;
+    public bool HasDoubleJump = true;
+    public GameObject projectilePrefab;
+    public Camera mainCamera;
+
+    [SerializeField] private GameObject spawn;
+
+
+
+    [SerializeField] private float _jumpForce = 15f;
+    private bool jumped = false;
+    [SerializeField] private LayerMask _groundLayer;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        movementInput = ctx.ReadValue<Vector2>();
+        Debug.Log("talyor ewww");
+    }
+    public bool IsGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position + offset, boxsize, 0, -transform.up, castDistance, _groundLayer))
+        {
+            HasDoubleJump = true;
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + offset + Vector3.down * castDistance, boxsize);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //other.GetComponent<Enemy>().TakeDamage(damage);
+            Debug.Log("Enemy hit!");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocityX = movementInput.x;
+
+        // Apply velocity in the FixedUpdate for consistent physics interactions (FixedUpdate is called at a fixed interval)
+        if (jumped)
+        {
+            rb.linearVelocityY = _jumpForce;
+            jumped = false;
+        }
+    }
+
+    public void Aim(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<Vector2>() != Vector2.zero)
+        {
+            gun.transform.right = context.ReadValue<Vector2>();
+        }
+
+    }
+
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameObject proj = Instantiate(projectilePrefab, spawn.transform.position, Quaternion.identity);
+            proj.transform.right = gun.transform.right;
+        }
+    }
+
+    internal void TakeDamage(int contactDamage)
+    {
+        throw new NotImplementedException();
+    }
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1)
+        {
+            if (IsGrounded())
+            {
+                rb.linearVelocityY = _jumpForce;
+                
+            }
+            if (!IsGrounded())
+            {
+                if (HasDoubleJump == true)
+                {
+                    rb.linearVelocityY = _jumpForce;
+                    HasDoubleJump = false;
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/RianScripts/Projectiles.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = transform.right * 10f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("yeouch");
+            Destroy(gameObject);
+        }
+
+
+    }
+}
+
