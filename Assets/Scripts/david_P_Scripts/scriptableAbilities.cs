00e93bd1ce7a113bde0b6b27459270b733310b22
MUKILTEO\1725383 <189248david@gmail.com>
2026-01-29 13:24:33 -0800

    no message

--- Assets/Scripts/david_P_Scripts/scriptableAbilities.cs
+++ Assets/Scripts/david_P_Scripts/scriptableAbilities.cs
@@ -3,5 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "scriptableAbilities", menuName = "Scriptable Objects/scriptableAbilities")]
 public class scriptableAbilities : ScriptableObject
 {
-    
+    public GameObject knockbackUP;
+    public GameObject speedUP;
+    public GameObject attackSpeedUp;
+    public GameObject knockbackRes;
+
+
+
+
 }
+ 
