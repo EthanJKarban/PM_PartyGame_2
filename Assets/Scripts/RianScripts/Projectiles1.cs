9fbeae836bbe84f9475ce7f3307d9c025c652650
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2026-01-29 12:01:49 -0800

    added knockback (not working)

--- Assets/Scripts/RianScripts/Projectiles1.cs
+++ Assets/Scripts/RianScripts/Projectiles1.cs
@@ -1,15 +1,23 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Projectiles1 : MonoBehaviour
 {
+    private Collider2D col;
     private Rigidbody2D rb;
-    
+
+    public float knockback;
+    public float speed;
+    public float lifetime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        col = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, 2f);
+
+        Destroy(gameObject, lifetime);
     }
 
     // Update is called once per frame
@@ -20,7 +28,7 @@ public class Projectiles1 : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = transform.right * 10f;
+        rb.linearVelocity = transform.right * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,10 +44,21 @@ public class Projectiles1 : MonoBehaviour
         {
 
             Destroy(gameObject);
+            Knockback();
         }
 
+        Vector3 point = collision.ClosestPoint(transform.position);
+        Vector2 difference = (transform.position - point).normalized;
+
+        Vector2 launchangle = difference * knockback;
+
+        collision.attachedRigidbody.linearVelocity = launchangle;
 
     }
 
+        void Knockback()
+    {
+
+    }
 }
 
