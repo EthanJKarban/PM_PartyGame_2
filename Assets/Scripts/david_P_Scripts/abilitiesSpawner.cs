2bbafed6cc639d0a2f00b78aa1c158b4f57788ed
MUKILTEO\1725383 <189248david@gmail.com>
2026-01-29 12:05:52 -0800

    rng spawner for abilities

--- Assets/Scripts/david_P_Scripts/abilitiesSpawner.cs
+++ Assets/Scripts/david_P_Scripts/abilitiesSpawner.cs
@@ -1,16 +1,38 @@
+using NUnit.Framework;
+using System.Collections;
 using UnityEngine;
 
 public class abilitiesSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [HideInInspector] public float xpos;
+    [HideInInspector] public float ypos;
+    public float totalAbilities;
+    public float abilityChance;
+    public float rand = 15f;
+    public float awayFromPlayer;
+
+    public float spawntime;
+    public float spawnDelay;
+
+    private void Start()
     {
-        
+        List listAbilities = new List();
+        StartCoroutine(spawnNewAbility());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator spawnNewAbility()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawntime);
+            xpos = Random.Range(-rand, rand);
+            ypos = Random.Range(-rand, rand);
+            Vector2 abilityPos = new Vector2(xpos, ypos);
+            float distanceFromPlayer = Vector2.Distance(abilityPos, GameObject.FindGameObjectWithTag("Player").transform.position);
+            if (distanceFromPlayer >= awayFromPlayer)
+            {
+                
+            }
+        }
     }
 }
