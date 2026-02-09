82660bc39fc53f62a91387ad5069290ae129a178
MUKILTEO\1725383 <189248david@gmail.com>
2026-02-09 11:58:02 -0800

    no message

--- Assets/Scripts/RianScripts/Projectiles1.cs
+++ Assets/Scripts/RianScripts/Projectiles1.cs
@@ -13,6 +13,8 @@ public class Projectiles1 : MonoBehaviour
     public float speed;
     public float lifetime;
     public float knockbackTime;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -67,13 +69,13 @@ public class Projectiles1 : MonoBehaviour
         collision.attachedRigidbody.linearVelocity = launchangle;
         
 
-        if (collision.TryGetComponent(out Player player))
+        if (collision.TryGetComponent(out PlayerMovement player))
         {
             player.StartCoroutine(adjustWeight(player));
         }
     }
 
-    IEnumerator adjustWeight(Player player)
+    IEnumerator adjustWeight(PlayerMovement player)
     {
         player.weight = 0;
 
